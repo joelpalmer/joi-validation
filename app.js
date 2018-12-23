1527dcb254cecc2782928bfeaf6b8b4762102cb2
Joel Palmer <joelpalmer@gmail.com>
2018-12-23 11:21:14 -0600

    app setup

---
+++ app.js
@@ -0,0 +1,25 @@
+// load dependencies
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const routes = require('/routes');
+
+const app = express();
+const port = process.env.NODE_ENV || 3000;
+
+// app configurations
+app.set('port', port);
+
+// load middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// load API routes
+app.use('/', Routes);
+
+// establish http server connection
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+});
